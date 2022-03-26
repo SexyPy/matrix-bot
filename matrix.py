0f755e16f9e203d1a2244e8869da4ae410366689
SexyPy <iamnotivan@protonmail.com>
2022-03-26 01:41:01 +0100

    [UPDATE] Move program version to config file

--- matrix.py
+++ matrix.py
@@ -3,12 +3,11 @@ from services.config import Config
 
 class MatrixPy:
         def __init__(self) -> None:
-            self.version  = "1.0.0"
             self.config = Config().get_config()
             self.account = Account(self.config)
 
         def get_version(self):
-            return self.version
+            return self.config["version"]
 
         def login(self):
             return self.account.data
